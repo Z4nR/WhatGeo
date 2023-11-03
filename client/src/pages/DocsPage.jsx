50e3fe82494b51f663033b244f443a89420e4e3e
Zulham Ari Nur Ridhwan <zulham.ari@ecampus.ut.ac.id>
2023-11-03 16:18:04 +0700

    undo Docs Page

--- client/src/pages/DocsPage.jsx
+++ client/src/pages/DocsPage.jsx
@@ -1,16 +1,30 @@
-import { codeData } from '../utils/docs-data';
+import { useEffect } from 'react';
+import hljs from 'highlight.js';
+import { cityRoute, codeData, provRoute } from '../utils/docs-data';
+import TableRoute from '@/components/docs/TableRoute';
 import TableCode from '@/components/docs/TableCode';
 
 export default function DocsPage() {
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
+
   return (
     <div className="py-4 px-6">
       <TableCode data={codeData} />
+      <div className="mockup-code xs:w-fit xs:max-w-screen-sm pl-5">
+        <pre>
+          <code className="language-javascript hjls">
+            const base_url = `zulham.ahlitani.com/geo/v1`
+          </code>
+        </pre>
+      </div>
       <div className="divider" />
-      <iframe
-        title="WhatGeo Documentation"
-        src="https://zulham.ahlitani.com/geo/api-docs/"
-        className=" w-full h-screen border-none overflow-scroll"
-      />
+      <h1 className="text-lg font-bold">Router Provinsi</h1>
+      <TableRoute route={provRoute} />
+      <div className="divider" />
+      <h1 className="text-lg font-bold">Router Kota</h1>
+      <TableRoute route={cityRoute} />
     </div>
   );
 }
