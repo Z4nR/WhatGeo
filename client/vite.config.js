3116c45f455abef27109260a1d8f06f81ff68689
Zulham Ari Nur Ridhwan <zulham.ari@ecampus.ut.ac.id>
2023-11-03 16:04:25 +0700

    undo updt

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/WhatGeo/',
+  base: '/WhatGeo',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
