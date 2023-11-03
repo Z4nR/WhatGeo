a9aa45f2c12f03314f6c58eb1fb25028909cccde
Zulham Ari Nur Ridhwan <zulham.ari@ecampus.ut.ac.id>
2023-11-03 15:57:40 +0700

    update docs page

--- client/src/pages/DocsPage.jsx
+++ client/src/pages/DocsPage.jsx
@@ -6,6 +6,11 @@ export default function DocsPage() {
     <div className="py-4 px-6">
       <TableCode data={codeData} />
       <div className="divider" />
+      <iframe
+        title="WhatGeo Documentation"
+        src="https://zulham.ahlitani.com/geo/api-docs/"
+        className=" w-full h-screen border-none overflow-scroll"
+      />
     </div>
   );
 }
