6b23a380d9e64fe11d7de302c848d94bdc85435e
Zulham Ari Nur Ridhwan <zulham.ari@ecampus.ut.ac.id>
2023-11-03 16:41:15 +0700

    change position

--- client/src/pages/DocsPage.jsx
+++ client/src/pages/DocsPage.jsx
@@ -4,13 +4,13 @@ import TableCode from '@/components/docs/TableCode';
 export default function DocsPage() {
   return (
     <div className="py-4 px-6">
-      <TableCode data={codeData} />
-      <div className="divider" />
       <iframe
         title="WhatGeo Documentation"
         src="https://zulham.ahlitani.com/geo/api-docs/"
         className=" w-full h-screen border-none overflow-scroll"
       />
+      <div className="divider" />
+      <TableCode data={codeData} />
     </div>
   );
 }
