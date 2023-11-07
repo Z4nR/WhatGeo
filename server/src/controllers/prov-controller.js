659b0f495e0f79fd172f9cc7b14864d90f81dd1b
Z4nR <zulham.ari@ecampus.ut.ac.id>
2023-11-07 21:59:59 +0700

    undo updt limit

--- server/src/controllers/prov-controller.js
+++ server/src/controllers/prov-controller.js
@@ -2,7 +2,7 @@ const prov = require('../models/coordinate/Province');
 const client = require('../utils/redis');
 
 let pageNumber = 1;
-const limit = 5;
+const limit = 3;
 
 module.exports = {
   provPage: async (req, res) => {
