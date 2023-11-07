fda9d5ce0833c598332af0fb1c2d197ac016fd55
Zulham Ari Nur Ridhwan <zulham.ari@ecampus.ut.ac.id>
2023-11-07 21:31:05 +0700

    updt caching controller

--- server/src/controllers/caching-controller.js
+++ server/src/controllers/caching-controller.js
@@ -33,7 +33,7 @@ module.exports = {
     if (!Number.isNaN(page) && page > 0) pageNumber = page;
 
     try {
-      const data = await client.getEx(`prov-${island}${page}`);
+      const data = await client.getEx(`prov-${island}${page}`, { EX: 600 });
       if (!data) return next();
 
       const parseData = JSON.parse(data);
@@ -50,7 +50,7 @@ module.exports = {
     if (!Number.isNaN(page) && page > 0) pageNumber = page;
 
     try {
-      const data = await client.getEx(`prov-${id}`);
+      const data = await client.getEx(`prov-${id}`, { EX: 600 });
       if (!data) return next();
 
       const parseData = JSON.parse(data);
@@ -67,7 +67,7 @@ module.exports = {
     if (!Number.isNaN(page) && page > 0) pageNumber = page;
 
     try {
-      const data = await client.getEx(`city-${page}`);
+      const data = await client.getEx(`city-${page}`, { EX: 600 });
       if (!data) return next();
 
       const parseData = JSON.parse(data);
@@ -85,7 +85,7 @@ module.exports = {
     if (!Number.isNaN(page) && page > 0) pageNumber = page;
 
     try {
-      const data = await client.getEx(`city-${prov_id}${page}`);
+      const data = await client.getEx(`city-${prov_id}${page}`, { EX: 600 });
       if (!data) return next();
 
       const parseData = JSON.parse(data);
@@ -120,7 +120,7 @@ module.exports = {
     if (!Number.isNaN(page) && page > 0) pageNumber = page;
 
     try {
-      const data = await client.getEx(`city-${id}`);
+      const data = await client.getEx(`city-${id}`, { EX: 600 });
       if (!data) return next();
 
       const parseData = JSON.parse(data);
