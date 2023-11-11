9305ffef10a0a221a12c79aab4fc2331b2542381
Z4nR <ari.comunity1@gmail.com>
2023-11-11 13:46:14 +0700

    update redis

--- server/src/utils/redis.js
+++ server/src/utils/redis.js
@@ -1,6 +1,6 @@
 const { createClient } = require('redis');
 //const client = createClient({ url: 'redis://redis:6379' });
-const client = createClient({ url: process.env.REDIS_URL });
+const client = createClient({ url: 'redis://1n!Pe7AinD0@34.31.156.141:6379' });
 //const client = createClient();
 
 module.exports = client;
