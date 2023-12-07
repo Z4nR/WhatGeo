3a5ed4afd25e52b1b974ee6cf8dd44709b3c3afb
Z4nR <ari.comunity1@gmail.com>
2023-12-07 23:33:07 +0700

    setup example page

--- client/src/pages/ExamplePage.jsx
+++ client/src/pages/ExamplePage.jsx
@@ -1,3 +1,19 @@
 export default function ExamplePage() {
-  return <p className="text-md">Contoh</p>;
+  return (
+    <div className="py-4 px-6">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">
+            Shoes!
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
