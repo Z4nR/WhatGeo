9e2072441bff6a8c35666d4de274bf39f9c77255
Z4nR <ari.comunity1@gmail.com>
2023-12-07 23:28:01 +0700

    update example list

--- client/src/utils/example-list.js
+++ client/src/utils/example-list.js
@@ -2,7 +2,10 @@ export const exampleList = [
   {
     url: 'https://github.com/Z4nR/WhatGeo',
     title: 'GeoAPI Documentation',
-    language: 'Javascript',
+    language: [
+      'https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB',
+      'https://img.shields.io/badge/react_query-React_Query?style=for-the-badge&logo=react%20query&logoColor=white&color=%23FF4154',
+    ],
     demo: 'https://z4nr.github.io/WhatGeo/',
   },
 ];
