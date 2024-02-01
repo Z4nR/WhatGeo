5af7f0851154a3055f8895a38d18991358da05cd
Z4nR <ari.comunity1@gmail.com>
2024-02-01 09:20:05 +0700

    repositioning dotenv

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express'),
   cors = require('cors'),
   bodyParser = require('body-parser');
@@ -6,8 +8,6 @@ const app = express(),
   db = require('./db'),
   route = require('./routes'),
   client = require('./utils/redis');
-
-require('dotenv').config();
 
 const env = process.env.NODE_ENV;
 const port = process.env.PORT || 5000;
@@ -24,6 +24,9 @@ app.use(cors({ origin: '*', methods: ['GET'] }));
 //Route
 app.use('/v1', route);
 
+//DB Connection
+db();
+
 //Redis logging
 client.on('error', (err) => {
   console.log(err);
@@ -34,9 +37,6 @@ const redisConnect = async () => {
 };
 
 redisConnect();
-
-//DB Connection
-db();
 
 //Listen Port
 app.listen(port, () => {
