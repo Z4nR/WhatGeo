c44c1cde3bf88ea26f0a446b184a3cecbc22a83c
Z4nR <ari.comunity1@gmail.com>
2024-02-03 00:00:57 +0700

    update redis setup

--- server/src/utils/redis.js
+++ server/src/utils/redis.js
@@ -1,8 +1,7 @@
 const { createClient } = require('redis');
-//const client = createClient({ url: 'redis://redis:6379' });
+
 const client = createClient({
   url: `redis://default:${process.env.REDIS_PW}@${process.env.REDIS_HOST}:6379`,
 });
-//const client = createClient();
 
 module.exports = client;
