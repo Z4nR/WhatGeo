1f7db2f75e1d9924faab0d8ee2c1085811c42e38
Z4nR <ari.comunity1@gmail.com>
2024-03-26 05:06:59 +0700

    update CityDetail data schema

--- server/src/models/detail/CityDetail.js
+++ server/src/models/detail/CityDetail.js
@@ -3,10 +3,12 @@ const { Schema } = mongoose;
 
 const CityDetailSchema = new Schema({
   city_id: Number,
+  type: String,
   city: String,
   date_created: String,
   lat_city: Number,
   long_city: Number,
+  slogan: String,
   description: String,
   destinations: [
     {
