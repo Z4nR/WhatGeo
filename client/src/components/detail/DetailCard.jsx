f40a26b047dd930ef42e1af50d3eb0e1270406d4
Z4nR <ari.comunity1@gmail.com>
2024-03-30 04:55:33 +0700

    update detail card

--- client/src/components/detail/DetailCard.jsx
+++ client/src/components/detail/DetailCard.jsx
@@ -1,4 +1,4 @@
-import { getProvDetailById } from '@/utils/network';
+import { getCityDetailById, getProvDetailById } from '@/utils/network';
 import { useQuery } from '@tanstack/react-query';
 
 export function DetailCardProv({ setDetail, provId }) {
@@ -9,7 +9,7 @@ export function DetailCardProv({ setDetail, provId }) {
   });
 
   return (
-    <div className="card w-full mt-4 bg-base-100 shadow-lg">
+    <div className="card w-full mt-4 bg-gray-100 shadow-lg">
       <div className="card-body">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -34,21 +34,32 @@ export function DetailCardProv({ setDetail, provId }) {
         </div>
         <div className="divider mt-0 mb-0" />
         <div className="flex w-full">
-          <div className="flex-grow-0 card">
+          <div className="flex-1 card">
             <h3 className="font-medium text-center mb-2">Detil</h3>
-            <h4 className="text-sm">
-              Diresmikan : <b>{data?.date_created}</b>
-            </h4>
-            <h4 className="text-sm">
-              Ibu kota : <b>{data?.capital}</b>{' '}
-              <i className=" text-xs">
-                ({data?.lat}, {data?.long})
-              </i>
-            </h4>
-            <h4 className="text-sm">Slogan :</h4>
+            <div className="flex flex-row gap-1">
+              <div className="flex-grow-[2]">
+                <h4 className="text-sm">Diresmikan </h4>
+                <h4 className="text-sm">Ibukota </h4>
+                <h4 className="text-sm">Slogan </h4>
+              </div>
+              <div className="flex-grow-[8]">
+                <h4 className="text-sm">
+                  : <b>{data?.date_created}</b>
+                </h4>
+                <h4 className="text-sm">
+                  : <b>{data?.capital}</b>{' '}
+                  <i className=" text-xs">
+                    ({data?.lat}, {data?.long})
+                  </i>
+                </h4>
+                <h4 className="text-sm">
+                  : <b>{data?.slogan}</b>
+                </h4>
+              </div>
+            </div>
           </div>
           <div className="divider divider-horizontal"></div>
-          <div className="flex-grow card">
+          <div className="flex-1 card">
             <h3 className="font-medium text-center mb-2">Ringkasan</h3>
             <h4 className="text-sm">{data?.desc}</h4>
           </div>
@@ -58,24 +69,73 @@ export function DetailCardProv({ setDetail, provId }) {
   );
 }
 
-export function DetailCradCity() {
+export function DetailCardCity({ setDetail, cityId }) {
+  const { data } = useQuery({
+    queryKey: ['city-detail', cityId],
+    queryFn: async () => await getCityDetailById(cityId),
+    gcTime: Infinity,
+  });
+
   return (
-    <div className="card w-full mt-4 bg-base-100 shadow-lg">
+    <div className="card w-full mt-4 bg-gray-100 shadow-lg">
       <div className="card-body">
-        <h2 className="card-title justify-center">Kota Tangerang</h2>
-        <div className="divider mt-0 mb-0" />
-        <div className="collapse collapse-arrow bg-gray-100">
-          <input type="radio" name="my-accordion-2" />
-          <div className="collapse-title text-base font-medium">Deskripsi</div>
-          <div className="collapse-content">
-            <p>hello</p>
-          </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6 absolute right-5 cursor-pointer"
+          onClick={(e) => {
+            e.preventDefault();
+            setDetail(false);
+          }}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
+        </svg>
+        <div
+          className="card-title justify-center"
+          style={{ flexDirection: 'column' }}
+        >
+          <h2>
+            {data?.type} {data?.city}
+          </h2>
+          <i className=" text-xs">
+            ({data?.lat_city}, {data?.long_city})
+          </i>
         </div>
-        <div className="collapse collapse-arrow bg-gray-100">
-          <input type="radio" name="my-accordion-2" />
-          <div className="collapse-title text-base font-medium">Destinasi</div>
-          <div className="collapse-content">
-            <p>hello</p>
+        <div className="divider mt-0 mb-0" />
+        <div className="flex w-full">
+          <div className="flex-1 card">
+            <h3 className="font-medium text-center mb-2">Detil</h3>
+            <div className="flex flex-row gap-1">
+              <div className="flex-grow-[2]">
+                <h4 className="text-sm">Diresmikan </h4>
+                <h4 className="text-sm">Slogan </h4>
+                <h4 className="text-sm">Deskripsi </h4>
+              </div>
+              <div className="flex-grow-[8]">
+                <h4 className="text-sm">
+                  : <b>{data?.date_created}</b>
+                </h4>
+                <h4 className="text-sm">
+                  : <b>{data?.slogan}</b>
+                </h4>
+                <h4 className="text-sm">
+                  : <b>Lorem Ipsum</b>
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="divider divider-horizontal"></div>
+          <div className="flex-1 card">
+            <h3 className="font-medium text-center mb-2">
+              Destinasi Yang Bisa Dikunjungi
+            </h3>
           </div>
         </div>
       </div>
@@ -83,4 +143,4 @@ export function DetailCradCity() {
   );
 }
 
-export default { DetailCardProv, DetailCradCity };
+export default { DetailCardProv, DetailCardCity };
