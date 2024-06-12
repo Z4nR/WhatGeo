d3acb4d2bf51813320ef5a62aed6855216793b05
Z4nR <ari.comunity1@gmail.com>
2024-06-12 08:28:55 +0700

    update document page

--- client/src/pages/DocsPage.jsx
+++ client/src/pages/DocsPage.jsx
@@ -2,12 +2,12 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 import { codeData } from '../utils/docs-data';
 import TableCode from '@/components/docs/TableCode';
-import { swaggerJson } from '@/utils/swagger';
+import { swaggerSpec } from '@/utils/swagger';
 
 export default function DocsPage() {
   return (
     <div className="py-4 px-6">
-      <SwaggerUI spec={swaggerJson} />
+      <SwaggerUI spec={swaggerSpec} />
       <div className="divider" />
       <TableCode data={codeData} />
     </div>
